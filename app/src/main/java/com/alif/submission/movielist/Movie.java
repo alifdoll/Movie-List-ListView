70d73dfde3fefd3004857ce743de9d3732448430
alif <alifianfajarpratama@gmail.com>
2020-03-24 23:22:28 +0700

    Initial commit

---
+++ app/src/main/java/com/alif/submission/movielist/Movie.java
@@ -0,0 +1,104 @@
+package com.alif.submission.movielist;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Movie implements Parcelable {
+
+    private int photo;
+    private int directorPhoto;
+    private String name;
+    private String releaseDate;
+    private String directorName;
+    private String overview;
+
+
+
+    public Movie() {
+
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public int getDirectorPhoto() {
+        return directorPhoto;
+    }
+
+    public void setDirectorPhoto(int directorPhoto) {
+        this.directorPhoto = directorPhoto;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public void setDirectorName(String directorName) {
+        this.directorName = directorName;
+    }
+
+    protected Movie(Parcel in) {
+        photo = in.readInt();
+        directorPhoto = in.readInt();
+        name = in.readString();
+        directorName = in.readString();
+        releaseDate = in.readString();
+        overview = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(photo);
+        dest.writeInt(directorPhoto);
+        dest.writeString(name);
+        dest.writeString(directorName);
+        dest.writeString(releaseDate);
+        dest.writeString(overview);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+}
---
+++ app/src/main/java/com/alif/submission/movielist/MovieDetail.java
@@ -0,0 +1,77 @@
+package com.alif.submission.movielist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MovieDetail extends AppCompatActivity {
+
+    static final String EXTRA_MOVIE = "extra_movie";
+
+    public MovieDetail(){
+
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movie_detail);
+
+
+        ImageView ivMoviePhoto = findViewById(R.id.movie_img);
+        ImageView ivDirectPhoto = findViewById(R.id.movie_img_director);
+        ImageView ivMovieBg = findViewById(R.id.movie_poster_bg);
+        TextView tvMovieName = findViewById(R.id.movie_name);
+        TextView tvMovieOverview = findViewById(R.id.movie_synops);
+        TextView tvMovieReleaseDate = findViewById(R.id.tv_movie_release_date);
+        TextView tvDirectName = findViewById(R.id.tv_director_name);
+
+        RatingBar rb = findViewById(R.id.movie_rating);
+        rb.setRating(5);
+
+        Movie movie = getIntent().getParcelableExtra(EXTRA_MOVIE);
+
+        tvMovieName.setText(movie.getName());
+        tvMovieOverview.setText(movie.getOverview());
+        tvMovieReleaseDate.setText(movie.getReleaseDate());
+        tvDirectName.setText(movie.getDirectorName());
+
+        ivMoviePhoto.setImageResource(movie.getPhoto());
+        ivDirectPhoto.setImageResource(movie.getDirectorPhoto());
+        ivMovieBg.setImageResource(movie.getPhoto());
+
+        getSupportActionBar().setTitle(movie.getName());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        this.overridePendingTransition(R.anim.enter_from_right,R.anim.exit_to_left);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent = new Intent(MovieDetail.this,MainActivity.class);
+        startActivity(intent);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        MovieDetail.this.overridePendingTransition(R.anim.enter_from_left,R.anim.exit_to_right);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+
+    }
+
+
+}
