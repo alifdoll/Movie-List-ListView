248ed67498ee4153cfc427bb42b8ac2c00fe3fc9
alif <alifianfajarpratama@gmail.com>
2020-07-12 15:53:46 +0700

    Adding Search Menu

--- app/src/main/java/com/alif/submission/movielist/MainActivity.java
+++ app/src/main/java/com/alif/submission/movielist/MainActivity.java
@@ -1,8 +1,11 @@
 package com.alif.submission.movielist;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -28,6 +31,26 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        MenuItem item = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) item.getActionView();
+        setupSearch(searchView);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    private void setupSearch(SearchView searchView) {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
 }
